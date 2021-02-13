291662fff965170fac57dcd91939365a28f4de08
Joe Schneider <schne152@mail.chapman.edu>
2021-02-13 11:52:44 -0700

    implemented navBar and sideBar

--- public/main.js
+++ public/main.js
@@ -10,3 +10,17 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+function toggleSidebar() {
+  if (document.getElementById("mediaNavToggler").className == "fas fa-bars") {
+    document.getElementById("mySidebar").style.width = "auto";
+    var width = document.getElementById("mySidebar").offsetWidth;
+    document.getElementById("navBar").style.marginLeft = width.toString()+"px";
+    document.getElementById("mediaNavToggler").className = "fas fa-times";
+  }
+  else {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("navBar").style.marginLeft = "0";
+    document.getElementById("mediaNavToggler").className = "fas fa-bars";
+  }
+}
